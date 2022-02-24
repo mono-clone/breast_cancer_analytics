d7d27e330b852be8ea8ff60dbfd6d259f03ee33e
masanari <mnojima0923@gmail.com>
2022-02-24 17:41:46 +0900

    first commit

---
+++ notebooks/config.py
@@ -0,0 +1,5 @@
+DATA_DIR='../data'
+BRCA_METABRIC_DATA_CLNICAL_DIR=DATA_DIR+'/brca_metabric_data_clinical'
+
+PANDAS_PROFILING_REPORT_DIR='./pandas-profiling_report'
+SWEETVIZ_REPORT_DIR='./sweetviz_report'
