1f5fa94e6f476b8dacac82dff80b4161fa45c3bc
masanari <mnojima0923@gmail.com>
2022-04-10 00:13:52 +0900

    add read tsv

--- notebooks/config.py
+++ notebooks/config.py
@@ -4,17 +4,17 @@ EXTERNAL_DIR = DATA_DIR + "/external"
 INTERIM_DIR = DATA_DIR + "/interim"
 PROCESSED_DIR = DATA_DIR + "/processed"
 RAW_DIR = DATA_DIR + "/raw"
-BRCA_METABRIC_DIR=RAW_DIR+"/brca_metabric"
+BRCA_METABRIC_DIR = RAW_DIR + "/brca_metabric"
 
 BRCA_METABRIC_DATA_CLNICAL_DIR = EXTERNAL_DIR + "/brca_metabric_data_clinical"
 
 REPORT_DIR = "../reports"
-FIGURES_DIR=REPORT_DIR+"/figures"
+FIGURES_DIR = REPORT_DIR + "/figures"
 
-SEABORN_DIR=FIGURES_DIR+"/sns"
+SEABORN_DIR = FIGURES_DIR + "/sns"
 PANDAS_PROFILING_REPORT_DIR = REPORT_DIR + "/pandas-profiling_report"
 SWEETVIZ_REPORT_DIR = REPORT_DIR + "/sweetviz_report"
 
 
 # FILEs
-RAW_BRCA_METABRIC_TG=RAW_DIR+'/brca_metabric.tar.gz'
+RAW_BRCA_METABRIC_TG = RAW_DIR + "/brca_metabric.tar.gz"
