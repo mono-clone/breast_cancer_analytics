bab925abf075eea589be151896f3dbbbf176775a
masanari <mnojima0923@gmail.com>
2022-04-11 01:08:17 +0900

    output header as csv

--- notebooks/config.py
+++ notebooks/config.py
@@ -1,16 +1,24 @@
 # DIRs
+# basic dirs
 DATA_DIR = "../data"
 EXTERNAL_DIR = DATA_DIR + "/external"
 INTERIM_DIR = DATA_DIR + "/interim"
 PROCESSED_DIR = DATA_DIR + "/processed"
 RAW_DIR = DATA_DIR + "/raw"
+
+# raw dirs
 BRCA_METABRIC_DIR = RAW_DIR + "/brca_metabric"
 
+# external dirs
 BRCA_METABRIC_DATA_CLNICAL_DIR = EXTERNAL_DIR + "/brca_metabric_data_clinical"
 
+# interim dirs
+INTERUM_BRCA_METABRIC_DIR = INTERIM_DIR + "/brca_metabric"
+
+
+# output dirs
 REPORT_DIR = "../reports"
 FIGURES_DIR = REPORT_DIR + "/figures"
-
 SEABORN_DIR = FIGURES_DIR + "/sns"
 PANDAS_PROFILING_REPORT_DIR = REPORT_DIR + "/pandas-profiling_report"
 SWEETVIZ_REPORT_DIR = REPORT_DIR + "/sweetviz_report"
