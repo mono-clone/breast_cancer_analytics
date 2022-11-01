8b54369dc8b1ed90e1c03ef46c60ea3008f9088e
nojima-WSL <mnojima0923@gmail.com>
2022-11-01 14:00:53 +0900

    リファクタリング

--- notebooks/config.py
+++ notebooks/config.py
@@ -10,75 +10,18 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.linear_model import SGDClassifier
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis as QDA  # 二次判別分析
 
-# ======================================================================================================
+# ######################################################################################################
 # データを保存するための最上位パス
 DATA_DIR = "../data"
-# ------------------------------------------------------------------------------------------------------
+# ======================================================================================================
+## 本番環境の前処理データ
+PROCESSED_DIR = DATA_DIR + "/processed"
+# ======================================================================================================
 ## 外部データの保存先の最上位パス
 EXTERNAL_DIR = DATA_DIR + "/external"
 ### metabricの臨床データを保存
 EXTERNAl_BRCA_METABRIC_DATA_CLNICAL_DIR = EXTERNAL_DIR + "/brca_metabric_data_clinical"
-# ------------------------------------------------------------------------------------------------------
-## 加工したデータ保存先の最上位パス
-INTERIM_DIR = DATA_DIR + "/interim"
 # ======================================================================================================
-### 1.X.X-EDAのデータ保存先の最上位パス
-INTERIM_EDA_DIR = INTERIM_DIR + "/EDA"
-# ......................................................................................................
-### 2.X.X-preprocessのデータ保存先の最上位パス
-INTERIM_PREPROCESSED_DIR = INTERIM_DIR + "/PREPROCESSED"
-#### 生存期間の保存先のパス
-INTERIM_PREPROCESSED_PROGNOSIS_DIR = (
-    INTERIM_PREPROCESSED_DIR + "/PROGNOSIS"
-)
-##### 臨床・遺伝子・混合データの保存先のパス
-INTERIM_PREPROCESSED_PROGNOSIS_CLINICAL_DIR = (
-    INTERIM_PREPROCESSED_PROGNOSIS_DIR + "/CLINICAL"
-)
-INTERIM_PREPROCESSED_PROGNOSIS_GENES_DIR = (
-    INTERIM_PREPROCESSED_PROGNOSIS_DIR + "/GENES"
-)
-INTERIM_PREPROCESSED_PROGNOSIS_CROSS_DIR = (
-    INTERIM_PREPROCESSED_PROGNOSIS_DIR + "/CROSS"
-)
-
-#### 再発の保存先のパス
-INTERIM_PREPROCESSED_RECURRENCE_DIR = INTERIM_PREPROCESSED_DIR + "/RECURRENCE"
-##### 混合データの保存先のパス
-INTERIM_PREPROCESSED_RECURRENCE_CROSS_DIR = (
-    INTERIM_PREPROCESSED_RECURRENCE_DIR + "/CROSS"
-)
-# ......................................................................................................
-### 3.X.X-create_bcmのデータ保存先の最上位パス
-INTERIM_MODELS_DIR = INTERIM_DIR + "/MODELS"
-INTERIM_MODELS_RECURRENCE_DIR = INTERIM_MODELS_DIR + "/RECURRENCE"
-INTERIM_MODELS_RECURRENCE_CROSS_DIR = INTERIM_MODELS_RECURRENCE_DIR + "/CROSS"
-# ......................................................................................................
-### 4.X.X-tuningのデータ保存先の最上位パス
-INTERIM_TUNING_DIR = INTERIM_DIR + "/TUNING"
-#### 生存期間の保存先のパス
-INTERIM_TUNING_PROGNOSIS_DIR = (
-    INTERIM_TUNING_DIR + "/PROGNOSIS"
-)
-##### 混合データの保存先のパス
-INTERIM_TUNING_PROGNOSIS_CROSS_DIR = (
-    INTERIM_TUNING_PROGNOSIS_DIR + "/CROSS"
-)
-#### 再発の保存先のパス
-INTERIM_TUNING_RECURRENCE_DIR = (
-    INTERIM_TUNING_DIR + "/RECURRENCE"
-)
-##### 混合データの保存先のパス
-INTERIM_TUNING_RECURRENCE_CROSS_DIR = (
-    INTERIM_TUNING_RECURRENCE_DIR + "/CROSS"
-)
-# ======================================================================================================
-### その他のデータを保存するためのパス
-INTERIM_OTHERS_DIR = INTERIM_DIR + "/OTHERS"
-# ------------------------------------------------------------------------------------------------------
-## 本番環境の前処理データ
-PROCESSED_DIR = DATA_DIR + "/processed"
-# ------------------------------------------------------------------------------------------------------
 ## 生データ
 RAW_DIR = DATA_DIR + "/raw"
 # ......................................................................................................
@@ -87,118 +30,76 @@ RAW_BRCA_METABRIC_DIR = RAW_DIR + "/brca_metabric"
 ### metabricの.gzファイル
 RAW_BRCA_METABRIC_TG = RAW_DIR + "/brca_metabric.tar.gz"
 # ======================================================================================================
+## 加工したデータ保存先の最上位パス
+INTERIM_DIR = DATA_DIR + "/interim"
+### 1-EDAのデータ保存先の最上位パス
+INTERIM_EDA_DIR = INTERIM_DIR + "/EDA"
+# ......................................................................................................
+### 2-preprocessのデータ保存先の最上位パス
+INTERIM_PREPROCESSED_DIR = INTERIM_DIR + "/PREPROCESSED"
+#### 再発の保存先のパス
+INTERIM_PREPROCESSED_RECURRENCE_DIR = INTERIM_PREPROCESSED_DIR + "/RECURRENCE"
+# ......................................................................................................
+### 3-create_bcmのデータ保存先の最上位パス
+INTERIM_MODELS_DIR = INTERIM_DIR + "/MODELS"
+INTERIM_MODELS_RECURRENCE_DIR = INTERIM_MODELS_DIR + "/RECURRENCE"
+# ......................................................................................................
+### 4-tuningのデータ保存先の最上位パス
+INTERIM_TUNING_DIR = INTERIM_DIR + "/TUNING"
+#### 再発の保存先のパス
+INTERIM_TUNING_RECURRENCE_DIR = (
+    INTERIM_TUNING_DIR + "/RECURRENCE"
+)
+### その他のデータを保存するためのパス
+INTERIM_OTHERS_DIR = INTERIM_DIR + "/OTHERS"
+# ######################################################################################################
 
 
-# ======================================================================================================
+# ######################################################################################################
 # 学習したモデルを保存するためのディレクトリパス
 MODELS = "../models"
 # ------------------------------------------------------------------------------------------------------
 ## ノートブックで学習したモデル
 MODELS_NOTEBOOK = MODELS + "/notebooks"
-# ======================================================================================================
+# ######################################################################################################
 
 
-# ======================================================================================================
+# ######################################################################################################
 # 分析結果の最上位パス
 REPORT_DIR = "../reports"
-# ------------------------------------------------------------------------------------------------------
+# ======================================================================================================
 ## 画像の保存先の最上位パス
 FIGURES_DIR = REPORT_DIR + "/FIGURES"
 # ......................................................................................................
-### 1.X.X-EDAで生成された画像保存先の最上位パス
+### 1-EDAで生成された画像保存先の最上位パス
 FIGURES_EDA_DIR = FIGURES_DIR + "/EDA"
-#### 生存期間の保存先のパス
-FIGURES_PROGNOSIS_DIR = FIGURES_EDA_DIR + "/PROGNOSIS"
-##### 臨床・遺伝子・混合データの保存先のパス
-FIGURES_PROGNOSIS_CLINICAL_DIR = FIGURES_PROGNOSIS_DIR + "/CLINICAL"
-FIGURES_PROGNOSIS_GENES_DIR = FIGURES_PROGNOSIS_DIR + "/GENES"
-FIGURES_PROGNOSIS_CROSS_DIR = FIGURES_PROGNOSIS_DIR + "/CROSS"
 #### 再発の保存先のパス
 FIGURES_RECURRENCE_DIR = FIGURES_EDA_DIR + "/RECURRENCE"
-###### 混合データのの保存先のパス
-FIGURES_RECURRENCE_CROSS_DIR = FIGURES_RECURRENCE_DIR + "/CROSS"
-
 # ......................................................................................................
-### 2.X.X-preprocessで生成された画像保存先の最上位パス
+### 2-preprocessで生成された画像保存先の最上位パス
 FIGURES_PREPROCESS_DIE = FIGURES_DIR + "/PREPROCESS"
-#### 生存期間の保存先のパス
-FIGURES_PREPROCESS_PROGNOSIS_DIR = FIGURES_PREPROCESS_DIE + "/PROGNOSIS"
-##### 臨床・遺伝子・混合データの保存先のパス
-FIGURES_PREPROCESS_PROGNOSIS_CLINICAL_DIR = (
-    FIGURES_PREPROCESS_PROGNOSIS_DIR + "/CLINICAL"
-)
-FIGURES_PREPROCESS_PROGNOSIS_GENES_DIR = FIGURES_PREPROCESS_PROGNOSIS_DIR + "/GENES"
-FIGURES_PREPROCESS_PROGNOSIS_CROSS_DIR = FIGURES_PREPROCESS_PROGNOSIS_DIR + "/CROSS"
 #### 再発の保存先のパス
 FIGURES_PREPROCESS_RECURRENCE_DIR = FIGURES_PREPROCESS_DIE + "/RECURRENCE"
-###### 混合データの保存先のパス
-FIGURES_PREPROCESS_RECURRENCE_CROSS_DIR = FIGURES_PREPROCESS_RECURRENCE_DIR + "/CROSS"
 # ......................................................................................................
-### 3.X.X-create_bcmで生成された画像保存先の最上位パス
+### 3-create_bcmで生成された画像保存先の最上位パス
 FIGURES_MODELS_DIR = FIGURES_DIR + "/MODELS"
-#### 生存期間の保存先のパス
-FIGURES_MODELS_PROGNOSIS_DIR = FIGURES_MODELS_DIR + "/PROGNOSIS"
-##### 臨床・遺伝子・混合データの保存先のパス
-FIGURES_MODELS_PROGNOSIS_CLINICAL_DIR = (
-    FIGURES_MODELS_PROGNOSIS_DIR + "/CLINICAL"
-)
-FIGURES_MODELS_PROGNOSIS_GENES_DIR = (
-    FIGURES_MODELS_PROGNOSIS_DIR + "/GENES"
-)
-FIGURES_MODELS_PROGNOSIS_CROSS_DIR = (
-    FIGURES_MODELS_PROGNOSIS_DIR + "/CROSS"
-)
 #### 再発の保存先のパス
 FIGURES_MODELS_RECURRENCE_DIR = FIGURES_MODELS_DIR + "/RECURRENCE"
-##### 混合データの保存先のパス
-FIGURES_MODELS_RECURRENCE_CROSS_DIR = (
-    FIGURES_MODELS_RECURRENCE_DIR + "/CROSS"
-)
 # ......................................................................................................
-### 4.X.X-tuningで生成された画像保存先の最上位パス
+### 4-tuningで生成された画像保存先の最上位パス
 FIGURES_TUNING_DIR = FIGURES_DIR + "/TUNING"
-#### 生存期間の保存先のパス
-FIGURES_TUNING_PROGNOSIS_DIR = FIGURES_TUNING_DIR + "/PROGNOSIS"
-##### 混合データの保存先のパス
-FIGURES_TUNING_PROGNOSIS_CROSS_DIR = (
-    FIGURES_TUNING_PROGNOSIS_DIR + "/CROSS"
-)
 #### 再発の保存先のパス
 FIGURES_TUNING_RECURRENCE_DIR = FIGURES_TUNING_DIR + "/RECURRENCE"
-##### 混合データの保存先のパス
-FIGURES_TUNING_RECURRENCE_CROSS_DIR = (
-    FIGURES_TUNING_RECURRENCE_DIR + "/CROSS"
-)
 # ......................................................................................................
-### 5.X.X-explainで生成された画像保存先の最上位パス
+### 5-explainで生成された画像保存先の最上位パス
 FIGURES_EXPLAIN_DIR = FIGURES_DIR + "/EXPLAIN"
-#### 生存期間の保存先のパス
-FIGURES_EXPLAIN_PROGNOSIS_DIR = FIGURES_EXPLAIN_DIR + "/PROGNOSIS"
-##### 混合データの保存先のパス
-FIGURES_EXPLAIN_PROGNOSIS_CROSS_DIR = (
-    FIGURES_EXPLAIN_PROGNOSIS_DIR + "/CROSS"
-)
 #### 再発のモデル作成で生成れた
 FIGURES_EXPLAIN_RECURRENCE_DIR = FIGURES_EXPLAIN_DIR + "/RECURRENCE"
-##### 混合データの保存先のパス
-FIGURES_EXPLAIN_RECURRENCE_CROSS_DIR = (
-    FIGURES_EXPLAIN_RECURRENCE_DIR + "/CROSS"
-)
 # ......................................................................................................
-### 6.X.X-lifelinesで生成された画像保存先の最上位パス
+### 6-lifelinesで生成された画像保存先の最上位パス
 FIGURES_LIFELINES_DIR = FIGURES_DIR + "/LIFELINES"
-#### 生存期間の保存先のパス
-FIGURES_LIFELINES_PROGNOSIS_DIR = FIGURES_LIFELINES_DIR + "/PROGNOSIS"
-##### 混合データの保存先のパス
-FIGURES_LIFELINES_PROGNOSIS_CROSS_DIR = (
-    FIGURES_LIFELINES_PROGNOSIS_DIR + "/CROSS"
-)
 #### 再発の保存先のパス
 FIGURES_LIFELINES_RECURRENCE_DIR = FIGURES_LIFELINES_DIR + "/RECURRENCE"
-##### 混合データの保存先のパス
-FIGURES_LIFELINES_RECURRENCE_CROSS_DIR = (
-    FIGURES_LIFELINES_RECURRENCE_DIR + "/CROSS"
-)
 # ......................................................................................................
 ### その他の画像保存先パス
 FIGURES_OTHERS_DIR = FIGURES_DIR + "/OTHERS"
@@ -207,65 +108,29 @@ FIGURES_OTHERS_DIR = FIGURES_DIR + "/OTHERS"
 FIGURES_DIR = REPORT_DIR + "/FIGURES"
 TABLES_DIR = REPORT_DIR + "/TABLES"
 # ......................................................................................................
-### 3.X.X-create_bcmで生成された表保存先の最上位パス
+### 3-create_bcmで生成された表保存先の最上位パス
 TABLES_MODELS_DIR = TABLES_DIR + "/MODELS"
-#### 生存期間のモデル作成で生成れた表
-TABLES_MODELS_PROGNOSIS_DIR = TABLES_MODELS_DIR + "/PROGNOSIS"
-##### 臨床・遺伝子・混合データの保存先のパス
-TABLES_MODELS_PROGNOSIS_CLINICAL_DIR = (
-    TABLES_MODELS_PROGNOSIS_DIR + "/CLINICAL"
-)
-TABLES_MODELS_PROGNOSIS_GENES_DIR = (
-    TABLES_MODELS_PROGNOSIS_DIR + "/GENES"
-)
-TABLES_MODELS_PROGNOSIS_CROSS_DIR = (
-    TABLES_MODELS_PROGNOSIS_DIR + "/CROSS"
-)
 #### 再発の保存先のパス
 TABLES_MODELS_RECURRENCE_DIR = TABLES_MODELS_DIR + "/RECURRENCE"
-##### 混合データの保存先のパス
-TABLES_MODELS_RECURRENCE_CROSS_DIR = (
-    TABLES_MODELS_RECURRENCE_DIR + "/CROSS"
-)
 # ......................................................................................................
-###  5.X.X-explainで生成された表保存先の最上位パス
+###  5-explainで生成された表保存先の最上位パス
 TABLES_EXPLAIN_DIR = TABLES_DIR + "/EXPLAIN"
-#### 生存期間のモデル作成で生成れた表
-TABLES_EXPLAIN_PROGNOSIS_DIR = TABLES_EXPLAIN_DIR + "/PROGNOSIS"
-##### 混合データの保存先のパス
-TABLES_EXPLAIN_PROGNOSIS_CROSS_DIR = (
-    TABLES_EXPLAIN_PROGNOSIS_DIR + "/CROSS"
-)
 #### 再発の保存先のパス
 TABLES_EXPLAIN_RECURRENCE_DIR = TABLES_EXPLAIN_DIR + "/RECURRENCE"
-##### 混合データの保存先のパス
-TABLES_EXPLAIN_RECURRENCE_CROSS_DIR = (
-    TABLES_EXPLAIN_RECURRENCE_DIR + "/CROSS"
-)
-# ======================================================================================================
+# ######################################################################################################
 
 
-# =====================================================================================================
+# ######################################################################################################
 # URL
 URL_cBioPortal = "https://cbioportal-datahub.s3.amazonaws.com/brca_metabric.tar.gz"
-# =====================================================================================================
+# ######################################################################################################
 
 
-# =====================================================================================================
+# ######################################################################################################
 # SEED
 SEED = 100
-# microarray_name
-SET_NAME_MICROARRAY = (
-    "mrna_agilent_microarray",
-    "mrna_agilent_microarray_zscores_ref_all_samples",
-    "mrna_agilent_microarray_zscores_ref_diploid_samples",
-)
-INDEX_MICROARRAY = 1
 THRESHOLD_YEARS=5
 THRESHOLD_MONTHS=THRESHOLD_YEARS*12
-# =====================================================================================================
-
-
 # =====================================================================================================
 classifiers = [
     LogisticRegression(max_iter=2000, random_state=SEED),
@@ -286,4 +151,4 @@ classifiers = [
     SGDClassifier(random_state=SEED, class_weight= "balanced"),
     QDA(),
 ]
-# =====================================================================================================
+# ######################################################################################################
